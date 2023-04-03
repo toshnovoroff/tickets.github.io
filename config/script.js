9faff6e7a28f307b1cfaa38fe57421ddc348d5dc
toshnovoroff <125685588+toshnovoroff@users.noreply.github.com>
2023-04-03 20:43:19 +0300

    Create script.js

---
+++ config/script.js
@@ -0,0 +1,297 @@
+const btn = document.querySelector('.nav-but')
+// const blackbut = document.querySelector('.black-but')
+const clsbtn = document.querySelector('.closetextone')
+const clsbtnone = document.querySelector('.closetexttwo')
+const switchone = document.querySelector('.tologin')
+const switchtwo = document.querySelector('.tosign')
+const modal = document.querySelector('.signupdiv')
+const modalq = document.querySelector('.signup')
+const modalone = document.querySelector('.logindiv')
+const modalqone = document.querySelector('.login')
+
+//plan section
+const firstarrow = document.querySelector('.firstarrow')
+const firstcard = document.querySelector('.firstcard')
+const firstbigcard = document.querySelector('.firstbigcard')
+const firstclosearrow = document.querySelector('.firstclosearrow')
+
+firstarrow.addEventListener('click', () =>{
+    firstcard.style.display = 'none';
+    firstbigcard.style.display = 'block';
+})
+
+firstclosearrow.addEventListener('click', () =>{
+    firstcard.style.display = 'block';
+    firstbigcard.style.display = 'none';
+})
+
+
+const secondarrow = document.querySelector('.secondarrow')
+const secondcard = document.querySelector('.secondcard')
+const secondbigcard = document.querySelector('.secondbigcard')
+const secondclosearrow = document.querySelector('.secondclosearrow')
+
+secondarrow.addEventListener('click', () =>{
+    secondcard.style.display = 'none';
+    secondbigcard.style.display = 'block';
+})
+
+secondclosearrow.addEventListener('click', () =>{
+    secondcard.style.display = 'block';
+    secondbigcard.style.display = 'none';
+})
+
+
+const thirdarrow = document.querySelector('.thirdarrow')
+const thirdcard = document.querySelector('.thirdcard')
+const thirdbigcard = document.querySelector('.thirdbigcard')
+const thirdclosearrow = document.querySelector('.thirdclosearrow')
+
+thirdarrow.addEventListener('click', () =>{
+    thirdcard.style.display = 'none';
+    thirdbigcard.style.display = 'block';
+})
+
+thirdclosearrow.addEventListener('click', () =>{
+    thirdcard.style.display = 'block';
+    thirdbigcard.style.display = 'none';
+})
+
+
+const fourtharrow = document.querySelector('.fourtharrow')
+const fourthcard = document.querySelector('.fourthcard')
+const fourthbigcard = document.querySelector('.fourthbigcard')
+const fourthclosearrow = document.querySelector('.fourthclosearrow')
+
+fourtharrow.addEventListener('click', () =>{
+    fourthcard.style.display = 'none';
+    fourthbigcard.style.display = 'block';
+})
+
+fourthclosearrow.addEventListener('click', () =>{
+    fourthcard.style.display = 'block';
+    fourthbigcard.style.display = 'none';
+})
+
+
+const fiftharrow = document.querySelector('.fiftharrow')
+const fifthcard = document.querySelector('.fifthcard')
+const fifthbigcard = document.querySelector('.fifthbigcard')
+const fifthclosearrow = document.querySelector('.fifthclosearrow')
+
+fiftharrow.addEventListener('click', () =>{
+    fifthcard.style.display = 'none';
+    fifthbigcard.style.display = 'block';
+})
+
+fifthclosearrow.addEventListener('click', () =>{
+    fifthcard.style.display = 'block';
+    fifthbigcard.style.display = 'none';
+})
+
+
+const sixtharrow = document.querySelector('.sixtharrow')
+const sixthcard = document.querySelector('.sixthcard')
+const sixthbigcard = document.querySelector('.sixthbigcard')
+const sixthclosearrow = document.querySelector('.sixthclosearrow')
+
+sixtharrow.addEventListener('click', () =>{
+    sixthcard.style.display = 'none';
+    sixthbigcard.style.display = 'block';
+})
+
+sixthclosearrow.addEventListener('click', () =>{
+    sixthcard.style.display = 'block';
+    sixthbigcard.style.display = 'none';
+})
+
+
+const seventharrow = document.querySelector('.seventharrow')
+const seventhcard = document.querySelector('.seventhcard')
+const seventhbigcard = document.querySelector('.seventhbigcard')
+const seventhclosearrow = document.querySelector('.seventhclosearrow')
+
+seventharrow.addEventListener('click', () =>{
+    seventhcard.style.display = 'none';
+    seventhbigcard.style.display = 'block';
+})
+
+seventhclosearrow.addEventListener('click', () =>{
+    seventhcard.style.display = 'block';
+    seventhbigcard.style.display = 'none';
+})
+
+
+const eighttharrow = document.querySelector('.eighttharrow')
+const eightthcard = document.querySelector('.eightthcard')
+const eightthbigcard = document.querySelector('.eightthbigcard')
+const eightthclosearrow = document.querySelector('.eightthclosearrow')
+
+eighttharrow.addEventListener('click', () =>{
+    eightthcard.style.display = 'none';
+    eightthbigcard.style.display = 'block';
+})
+
+eightthclosearrow.addEventListener('click', () =>{
+    eightthcard.style.display = 'block';
+    eightthbigcard.style.display = 'none';
+})
+
+
+const nintharrow = document.querySelector('.nintharrow')
+const ninthcard = document.querySelector('.ninthcard')
+const ninthbigcard = document.querySelector('.ninthbigcard')
+const ninthclosearrow = document.querySelector('.ninthclosearrow')
+
+nintharrow.addEventListener('click', () =>{
+    ninthcard.style.display = 'none';
+    ninthbigcard.style.display = 'block';
+})
+
+ninthclosearrow.addEventListener('click', () =>{
+    ninthcard.style.display = 'block';
+    ninthbigcard.style.display = 'none';
+})
+
+
+const tentharrow = document.querySelector('.tentharrow')
+const tenthcard = document.querySelector('.tenthcard')
+const tenthbigcard = document.querySelector('.tenthbigcard')
+const tenthclosearrow = document.querySelector('.tenthclosearrow')
+
+tentharrow.addEventListener('click', () =>{
+    tenthcard.style.display = 'none';
+    tenthbigcard.style.display = 'block';
+})
+
+tenthclosearrow.addEventListener('click', () =>{
+    tenthcard.style.display = 'block';
+    tenthbigcard.style.display = 'none';
+})
+
+
+const eleventharrow = document.querySelector('.eleventharrow')
+const eleventhcard = document.querySelector('.eleventhcard')
+const eleventhbigcard = document.querySelector('.eleventhbigcard')
+const eleventhclosearrow = document.querySelector('.eleventhclosearrow')
+
+eleventharrow.addEventListener('click', () =>{
+    eleventhcard.style.display = 'none';
+    eleventhbigcard.style.display = 'block';
+})
+
+eleventhclosearrow.addEventListener('click', () =>{
+    eleventhcard.style.display = 'block';
+    eleventhbigcard.style.display = 'none';
+})
+
+
+const twelvetharrow = document.querySelector('.twelvetharrow')
+const twelvethcard = document.querySelector('.twelvethcard')
+const twelvethbigcard = document.querySelector('.twelvethbigcard')
+const twelvethclosearrow = document.querySelector('.twelvethclosearrow')
+
+twelvetharrow.addEventListener('click', () =>{
+    twelvethcard.style.display = 'none';
+    twelvethbigcard.style.display = 'block';
+})
+
+twelvethclosearrow.addEventListener('click', () =>{
+    twelvethcard.style.display = 'block';
+    twelvethbigcard.style.display = 'none';
+})
+
+
+const thirteentharrow = document.querySelector('.thirteentharrow')
+const thirteenthcard = document.querySelector('.thirteenthcard')
+const thirteenthbigcard = document.querySelector('.thirteenthbigcard')
+const thirteenthclosearrow = document.querySelector('.thirteenthclosearrow')
+
+thirteentharrow.addEventListener('click', () =>{
+    thirteenthcard.style.display = 'none';
+    thirteenthbigcard.style.display = 'block';
+})
+
+thirteenthclosearrow.addEventListener('click', () =>{
+    thirteenthcard.style.display = 'block';
+    thirteenthbigcard.style.display = 'none';
+})
+
+
+const fourteentharrow = document.querySelector('.fourteentharrow')
+const fourteenthcard = document.querySelector('.fourteenthcard')
+const fourteenthbigcard = document.querySelector('.fourteenthbigcard')
+const fourteenthclosearrow = document.querySelector('.fourteenthclosearrow')
+
+fourteentharrow.addEventListener('click', () =>{
+    fourteenthcard.style.display = 'none';
+    fourteenthbigcard.style.display = 'block';
+})
+
+fourteenthclosearrow.addEventListener('click', () =>{
+    fourteenthcard.style.display = 'block';
+    fourteenthbigcard.style.display = 'none';
+})
+
+
+const fifteentharrow = document.querySelector('.fifteentharrow')
+const fifteenthcard = document.querySelector('.fifteenthcard')
+const fifteenthbigcard = document.querySelector('.fifteenthbigcard')
+const fifteenthclosearrow = document.querySelector('.fifteenthclosearrow')
+
+fifteentharrow.addEventListener('click', () =>{
+    fifteenthcard.style.display = 'none';
+    fifteenthbigcard.style.display = 'block';
+})
+
+fifteenthclosearrow.addEventListener('click', () =>{
+    fifteenthcard.style.display = 'block';
+    fifteenthbigcard.style.display = 'none';
+})
+
+
+btn.addEventListener('click', () =>{
+    modal.style.display = 'block';
+    modalq.style.display = 'block';
+})
+
+// blackbut.addEventListener('click', () =>{
+//     modal.style.display = 'block';
+//     modalq.style.display = 'block';
+// })
+
+clsbtn.addEventListener('click', () =>{
+    modal.style.display = 'none';
+})
+
+clsbtnone.addEventListener('click', () =>{
+    modalone.style.display = 'none';
+})
+
+switchone.addEventListener('click', () =>{
+    modal.style.display = 'none';
+    modalone.style.display = 'block';
+    modalqone.style.display = 'block';
+})
+
+switchtwo.addEventListener('click', () =>{
+    modalone.style.display = 'none';
+    modal.style.display = 'block';
+    modalq.style.display = 'block';
+})
+
+
+const whitedownarrow = document.querySelector('.whitedownarrow')
+const whiteuparrow = document.querySelector('.whiteuparrow')
+const closedlogobtndiv = document.querySelector('.closedlogobtndiv')
+const openednav = document.querySelector('.openednav')
+
+whitedownarrow.addEventListener('click', () =>{
+    closedlogobtndiv.style.display = 'none';
+    openednav.style.display = 'block';
+})
+
+whiteuparrow.addEventListener('click', () =>{
+    openednav.style.display = 'none';
+    closedlogobtndiv.style.display = 'flex';
+})
